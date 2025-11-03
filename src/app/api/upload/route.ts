98db81b64de4fabce7d27e638b865de7e6a405fb
JHyeon <jonghyeon88@gmail.com>
2025-11-03 17:49:26 +0900

    upload 프로세스에서 route페이지 리턴코드 오류 수정

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -10,10 +10,17 @@ export async function POST(request: Request): Promise<NextResponse> {
             { message: "파일이름이 없거나 전송되지 않았습니다." },
             { status: 400 }
         )
-
+    }
+    try {
         const blob = await put(filename, request.body, {
             access: "public",
         })
         return NextResponse.json(blob)
+    } catch (e) {
+        console.error("Vercel Blob 업로드 오류 : ", e)
+        return NextResponse.json(
+            { message: "파일 업로드 중 오류가 발생했습니다." },
+            { status: 500 }
+        )
     }
 }
