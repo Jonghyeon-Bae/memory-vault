8fde16b994dfee88fdc989f668e55f1875521401
JHyeon <jonghyeon88@gmail.com>
2025-11-04 16:06:53 +0900

    upload에서 blob할때 addRandomSuffix 옵션을 true로 줘서 중복회피 하도록 설정

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -14,6 +14,7 @@ export async function POST(request: Request): Promise<NextResponse> {
     try {
         const blob = await put(filename, request.body, {
             access: "public",
+            addRandomSuffix: true,
         })
         return NextResponse.json(blob)
     } catch (e) {
