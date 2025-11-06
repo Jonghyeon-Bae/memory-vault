897b3b9a80f6744becfb55fc3957990a62a71745
JHyeon <jonghyeon88@gmail.com>
2025-11-06 09:42:09 +0900

    servicekey 재삽입

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,9 @@ import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-// const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY
 
-// if (!supabaseUrl || !supabaseAnonKey || !supabaseServiceKey) {
-if (!supabaseUrl || !supabaseAnonKey) {
+if (!supabaseUrl || !supabaseAnonKey || !supabaseServiceKey) {
     throw new Error(
         "Supabase URL, Anon Key, 또는 Service Key 환경 변수가 설정되지 않았어요."
     )
@@ -13,7 +12,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
     auth: {
         autoRefreshToken: false,
         persistSession: false,
