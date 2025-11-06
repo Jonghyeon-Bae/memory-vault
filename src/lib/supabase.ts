3060e09e3251dd99a05cf432804b656f1abdee66
JHyeon <jonghyeon88@gmail.com>
2025-11-06 09:57:31 +0900

    auth 코드 삭제.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,9 +7,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL, Anon Key 환경 변수가 설정되지 않았어요.")
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: {
-        autoRefreshToken: false,
-        persistSession: false,
-    },
-})
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
